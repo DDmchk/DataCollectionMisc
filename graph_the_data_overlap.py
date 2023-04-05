ef461066dd938be8d3affd4b85bc4e493e2f8956
Dmitry Demchuk <demchuck@yahoo.com>
2023-04-05 19:15:34 +0300

    modified graph_the_data_overlap.py to show diff days by diff colors

--- graph_the_data_overlap.py
+++ graph_the_data_overlap.py
@@ -3,40 +3,59 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 x = []
-xstr =[]
 y = []
+colors = []
 
+color_selection = ['red', 'blue', 'green', 'brown', 'gray', 'violet', 'black', 'yellow']
+curcolor = 0
+sections = []
 with open('visitor_counts.csv', 'r') as file:
     reader = csv.reader(file)
 
     # Get the reference date (midnight of the first date in the dataset)
     first_row = next(reader)
     date_str = first_row[0].split(' ')[0]
+    prev_date_str = date_str
     ref_date = datetime.strptime(date_str, '%Y-%m-%d')
     ref_day = timedelta(days=1)
 
     # Read the rest of the rows and adjust the x-axis values to show only 24 hours
+    rowcount = 0
     for row in reader:
         try:
+            date_str = row[0].split(' ')[0]
             date_time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
             date_diff = date_time - ref_date  # Get difference from ref date
             total_seconds = date_diff.total_seconds()
             hours = (total_seconds // 3600) % 24  # Get hours since midnight
             minutes = (total_seconds % 3600) // 60
             seconds = total_seconds % 60
-            x.append(timedelta(hours=hours, minutes=minutes, seconds=seconds))
+            x.append(timedelta(hours=hours, minutes=minutes, seconds=seconds).seconds)
             y.append(int(row[1]))
-            xstr.append(timedelta(hours=hours, minutes=minutes, seconds=seconds).seconds)
+            # Use a different color for each day's data
+            if prev_date_str != date_str:
+                curcolor += 1
+                colors.append(color_selection[curcolor])
+                sections.append([rowcount,prev_date_str])
+                prev_date_str = date_str
+            else:
+                colors.append(color_selection[curcolor])
+            rowcount += 1
         except Exception as err:
+            print(err)
             continue
-
+    sections.append([rowcount-1,prev_date_str])
+    print(colors)
     # Plot the graph with overlapping datasets
-    plt.plot(xstr, y, label=date_str)
+    beginpref = 0
+    for s in sections:
+        plt.plot(x[beginpref:s[0]], y[beginpref:s[0]], color=colors[s[0]-1], label=s[1])
+        beginpref = s[0]
 
     # Adjust the x-axis ticks and labels
-    plt.xticks([x*3600 for x in range(0, 24)])
-    plt.xlabel('Time (hh:mm:ss)')
-    plt.ylabel('visitors')
-    plt.title("RESTART's Visitors vs Time Graph")
-    plt.legend(loc='upper left')
+    plt.xticks([x * 3600 for x in range(0, 24)])
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Visitors')
+    plt.title("RESTART's visitors vs Time Graph")
+    plt.legend(loc='best')
     plt.show()
