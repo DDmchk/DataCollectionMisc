ec3ade2538ac544659c51c86e552f59df1df8fdb
Dmitry Demchuk <demchuck@yahoo.com>
2023-04-05 16:29:04 +0300

    graph added. also added CSV to backup

---
+++ graph_the_data.py
@@ -0,0 +1,23 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+x = []
+y = []
+
+with open('visitor_counts.csv', 'r') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        try:
+            date_time_obj = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+            x.append(date_time_obj)
+            y.append(int(row[1]))
+        except Exception as err:
+            print(str(err))
+            continue
+plt.plot(x, y)
+plt.xlabel('Time')
+plt.ylabel('Data')
+plt.title("Restart's visitors vs Time Graph")
+plt.show()
